f7cf457e2b52f7a55905e90a95cb9980053cff59
dani koskas <koskas.dani@gmail.com>
2020-05-25 13:28:17 +0300

    adding def

--- index2.py
+++ index2.py
@@ -1,4 +1,21 @@
-# Triangle of pascal
 
+
+# Triangle of pascal
 def pascal():
     pass
+
+
+# just for fun
+
+def xor(x, y):
+    if num1 == True and num2 == True:
+        print('False')
+    elif num1 == False and num2 == False:
+        print('False')
+    elif num1 != False or num2 != False:
+        print("True")
+
+
+num1 = int(input("enter the first number \n"))
+num2 = int(input("enter the second number \n"))
+xor(num1, num2)
